63130cd5f339f473b02740819e894f240693c21a
Manuel98luna01~ <jmlzelaya22@gmail.com>
2023-06-26 21:59:52 -0600

    paso

---
+++ Domain/Primitives/AggregateRoot.cs
@@ -0,0 +1,29 @@
+namespace Domain.Primitives;
+
+
+ public abstract class AggregateRoot
+{
+
+    private readonly List<DomainEvent> _domainEvents = new();
+
+    public ICollection<DomainEvent> GetDomainEvents() => _domainEvents;
+
+    protected void Raise(DomainEvent domainevent)
+    {
+        _domainEvents.Add(domainevent);    
+    }
+}  
+
+
+
+
+
+
+
+
+
+
+
+
+       
+    
---
+++ Domain/Primitives/PhoneNumber.cs
@@ -0,0 +1,26 @@
+using System.Text.RegularExpressions;
+
+namespace Domain.ValueObjects;
+
+public partial record PhoneNumber
+{
+    private const int DefaultLenghs=9;
+    private const string Pattern =@"^(?:-*\d-*){8}$";
+    private PhoneNumber(string value) => Value = value;
+
+ public static PhoneNumber? Create(string value)
+    {
+        if(string.IsNullOrEmpty(value) || !PhoneNumberRegex().IsMatch(value)||value.Length != DefaultLenghs)
+        {
+            return null;
+        }
+        return new PhoneNumber(value);
+    } 
+
+    public string Value { get; set; }
+
+
+ [GeneratedRegex(Pattern)]
+ private static partial Regex PhoneNumberRegex();
+ 
+}
