31a509bd7ae10ad4380e78c72461d980c629fdc8
Manuel98luna01~ <jmlzelaya22@gmail.com>
2023-06-26 22:41:22 -0600

    paso2

---
+++ Domain/Customers/Customer.cs
@@ -0,0 +1,32 @@
+using Domain.Primitives;
+using Domain.ValueObjects;
+
+namespace Domain.Customers;
+
+public sealed class Customer : AggregateRoot
+{
+
+    public Customer(CustomerId id, string name, string description, string ubication, Address address, bool active)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+        Ubication = ubication;
+        Address = address;
+        Active = active;
+
+    }
+
+    private Customer(){}
+
+
+    public CustomerId Id{ get; private set; }
+    public string Name{ get; private set; } = string.Empty;
+    public string Description{ get; private set; }= string.Empty;
+
+    public string Ubication{ get; private set; }= string.Empty;
+
+    public Address Address{ get; private set; }
+    public bool Active{ get;  set; }
+
+}
